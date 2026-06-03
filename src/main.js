b0057f0b63201d3413ade05555e640140fc7c35f
https://github.com/Gabbar-Sharma <gabbarsharma75663@gmail.com>
2026-06-03 21:07:15 +0530

    add files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import './style.css'
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const list = document.querySelector('#ul')
+const listItem = document.querySelector('#li')  
+
+
+button.addEventListener('click', () =>{
+  const newListItem = document.createElement('li')
+  newListItem.innerText = input.value
+  list.appendChild(newListItem)
+  
+
+  
+
+  
+   input.value = " "
+
+
+})
+
