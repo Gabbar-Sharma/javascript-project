889d3ffa979c24a4bf5fb3a6c3f15b00893e2727
https://github.com/Gabbar-Sharma <gabbarsharma75663@gmail.com>
2026-06-03 21:09:59 +0530

    add comment

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ button.addEventListener('click', () =>{
   const newListItem = document.createElement('li')
   newListItem.innerText = input.value
   list.appendChild(newListItem)
-  
+  //yesterday I was trying to add the list item to the list but it was not working because I was trying to add the list item to the list item instead of the list. So I changed the code to add the list item to the list and it worked.
 
   
 
