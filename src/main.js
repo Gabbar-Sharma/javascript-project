146c132bbda6a66fec9b89eab201fdc8092b44fd
https://github.com/Gabbar-Sharma <gabbarsharma75663@gmail.com>
2026-06-04 08:02:57 +0530

    add files

--- src/main.js
+++ src/main.js
@@ -1,33 +1,20 @@
-import './style.css'
-const input = document.querySelector('input')
-const button = document.querySelector('button')
-const list = document.querySelector('#ul')
-const listItem = document.querySelector('#li')  
-
+import "./style.css";
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("#ul");
+const listItem = document.querySelector("#listItem");
 
 const addTodo = () => {
-   const newListItem = document.createElement('li')
-   newListItem.innerText = input.value
-   list.appendChild(newListItem)
+  const newListItem = document.createElement("listItem");
+  newListItem.innerText = input.value;
+  list.appendChild(newListItem);
+  input.value = " ";
+};
 
-  input.value = " "
-}
-
-button.addEventListener('click' , () => {
-  if( input.value === ""){
-    
+button.addEventListener("click", () => {
+  if (input.value === "" || input.value === " ") {
     return;
-  };
-  addTodo()
-  if( input.value.trim() === " "  ) return;
-
-  console.log(input.value)
-})
-  
-
-  
-   
-
-
-
-
+  }
+  addTodo();
+  if (input.value.trim() === " ") return;
+});
