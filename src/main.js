8095e1b1a2983472c98dca11bc6a6e6ba79ce691
https://github.com/Gabbar-Sharma <gabbarsharma75663@gmail.com>
2026-06-04 08:20:22 +0530

    add marked btn

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 const list = document.querySelector("#ul");
 const listItem = document.querySelector("#listItem");
+const deleteButton = document.querySelector("#delete");
+const editButton = document.querySelector("#edit");
+const markButton = document.querySelector("#mark");
 
 const addTodo = () => {
   const newListItem = document.createElement("listItem");
@@ -10,10 +13,28 @@ const addTodo = () => {
   list.appendChild(newListItem);
   input.value = " ";
 };
+const deleteTodo = () => {
+  list.removeChild(list.lastChild);
+};
 
 button.addEventListener("click", () => {
   if (input.value === "" || input.value === " " || input.value.trim() === " ") {
     return;
   }
   addTodo();
+
 });
+deleteButton.addEventListener("click", () => {
+  deleteTodo();
+});
+editButton.addEventListener("click", () => {
+  const lastListItem = list.currentChild;
+}
+);
+markButton.addEventListener("click", () => {
+  
+  listItem.style.textDecoration = "line-through";
+ 
+}
+);
+
